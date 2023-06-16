4a79cd5c6a8153fcb534cf9824558af72fa018fc
Pavel1995 <pavel1995over@gmail.com>
2023-06-16 07:36:16 +0300

    naDiplom

--- ApiShopTest/Controllers/CheckOrderController.cs
+++ ApiShopTest/Controllers/CheckOrderController.cs
@@ -21,7 +21,7 @@ namespace ApiShopTest.Controllers
         {
             var info = db.UsersProducts.FirstOrDefault(i => i.idUsers == Id && i.amountMin==i.amountCurrent);
             if (info != null)
-            {
+            { 
                 return Ok();
             }   
             else
